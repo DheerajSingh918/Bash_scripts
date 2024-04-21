c910cf7f2ac7660987697a24f03c1989b3da30ab
DheerajSingh918 <dheerajsinghph119@gmail.com>
2024-04-21 20:23:25 +0530

    Modfit the Server audit script..

--- Server_audit.sh
+++ Server_audit.sh
@@ -1,18 +1,31 @@
-#! /bin/bash/
+#! bin/bash/
+# Define color codes using output
+RED="\e[31m"
+RESET="\e[0m" 
+
 
 if [ $(id -u) -ne 0 ]; then
-  echo ("Not Running as Root user..")
+   echo "Not Running as Root user.."
   exit 1
 fi
 
-all_users=()
-Audit_FILE=Audit_server.txt
-touch Audit_server.txt
-echo "======={HOSTNAME}====================\n"
-printf "$(hostname)\n\n\n"
-#======={IP Address of this server}======================
-echo "======={IP Address of this server}======================\n\n\n"
-printf "$(curl -s ipconfig.io/ip)\n\n\n"
+all_user=()
+Audit_FILE=Audit.txt
+touch Audit.txt
+
+printf "$(echo ${RED}=============={HOSTNAME}====================${RESET}) \n\n" >> Audit.txt
+printf "$(hostname)\n\n"  >> Audit.txt
+
+
+printf "$(echo ${RED}============={IP Address of this server}==================${RESET}) \n\n" >>Audit.txt
+printf "$(curl -s ipconfig.io/ip)\n\n" >> Audit.txt 
+
+
+printf "$(echo ${RED}==================={OS-Details}=================== ${RESET})\n" >> Audit.txt
+printf "$(head -2 /etc/os-release) \n\n" >> Audit.txt
+
+printf "$(echo ${RED}=================={List of open port}=====================${RESET})\n" >>Audit.txt
+printf "$(netstat -ntlp | grep LISTEN) \n" >> Audit.txt
 
 
 
